7ac748734b9552c1f45e40bd29c8522b486f817d
Joost Lubach <joostlubach@gmail.com>
2024-03-29 14:34:24 +0100

    Converted to ESModules

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { default as DependencyProvider } from './DependencyProvider'
-export * from './types'
+export { default as DependencyProvider } from './DependencyProvider.js'
+export * from './types.js'
